bdae6cfcf4426fb198e7967120eaadc8d3471bd1
aewens <email@aewens.com>
2021-03-15 19:00:10 +0000

    Added attach_auth_params and AuthSession.do_api_request

--- python/api/session.py
+++ python/api/session.py
@@ -7,6 +7,13 @@
 #  MAX_REDIRECTS,
 #} from 'src/utils/constants';
 #import { request, RequestResponse } from './request';
+from aiohttp import ClientSession, CookieJar
+
+from collections import namedtuple
+from asyncio import get_event_loop
+from time import time
+from urllib.parse import urljoin
+from re import compile as regex
 
 # 5 minutes
 MIN_AUTH_LENGTH = 1000 * 60 * 5
@@ -24,114 +31,98 @@ def extract_and_mutate_search_params(attr, params):
 
     return ret
 
+AuthState = namedtuple("AuthState", [
+    "week_offset",
+    "params",
+    "auth_time"
+])
+
+def attach_auth_params(auth_state, include_session_params=False):
+    params = dict()
+
+    # TODO - Patch this to work with back-end web framework being used
+    auth_params = auth_state.params
+    for key in url.params.keys():
+        if key in auth_params:
+            del auth_params[key]
+
+    if include_session_params is True:
+        for key, value in auth_params.items():
+            params[key] = value
+
+    elif isinstance(include_session_params, list):
+        for key, value in auth_params.items():
+            if key in include_session_params:
+                params[key] = value
+
+    return params
+
+class AuthSession:
+    def __init__(self, username):
+        self.username = username
+        self._session = ClientSession(cookie_jar=CookieJar())
+
+        self._auth_state = None
+        self._auth_refresh_promise = None
+
+    async def do_api_request(
+        self,
+        req_url,
+        follow_redirects=False,
+        include_session_params=False,
+        **kwargs
+    ):
+        auth_state = self._auth_state
+        auth_time = self._auth_state.auth_time
+        if auth_state is None or auth_time + MAX_AUTH_LENGTH < time():
+            auth_state = await self.refresh_auth()
+
+        params = attach_auth_params(req_url, auth_state, include_session_params)
+        kwargs["params"] = params
+
+        url = req_url
+
+        res = None
+        redirects = 0
+        while True:
+            async with self._session as sess:
+                async with sess.get(url, follow_redirects, **kwargs) as res:
+                    if res.status not in [301, 302]:
+                        break
+
+                    if res.headers.get("location") is None:
+                        raise Exception("""
+                        Invalid redirect, location header not provided.
+                        """.strip())
+
+                    new_url = urljoin(url, r.headers.get("location"))
+                    pattern = regex(r"error\.aspx$")
+                    if pattern.search(new_url) is not None:
+                        if auth_time + MIN_AUTH_LENGTH > time():
+                            raise Exception("Server responded with an error")
+
+                        args = follow_redirect, include_session_params
+                        await self.refresh_auth()
+                        return self.do_api_request(req_url, *args, **kwargs)
+
+                    if not follow_redirects:
+                        break
+
+                    url = new_url
+                    params = attach_auth_params(url, auth_state,
+                        include_session_params)
+                    kwargs["params"] = params
+
+                    if redirects >= MAX_REDIRECTS:
+                        raise Exception(f"""
+                        Request failed, too many redirects: {req_url}
+                        """.strip())
+
+                    redirects = redirects + 1
+
+    return res
+
 """
-interface AuthState {
-  weekOffset: number;
-  searchParams: URLSearchParams;
-  authTime: number;
-}
-
-function attachAuthParams(
-  url: URL,
-  authState: AuthState,
-  includeSessionSearchParams?: string[] | boolean,
-) {
-  if (includeSessionSearchParams === true) {
-    const authParams = new URLSearchParams(authState.searchParams);
-    for (const key of url.searchParams.keys()) {
-      authParams.delete(key);
-    }
-    for (const [key, val] of authParams.entries()) {
-      url.searchParams.append(key, val);
-    }
-  }
-  if (Array.isArray(includeSessionSearchParams)) {
-    const authParams = new URLSearchParams(authState.searchParams);
-    for (const key of url.searchParams.keys()) {
-      authParams.delete(key);
-    }
-    for (const [key, val] of authParams.entries()) {
-      if (key in includeSessionSearchParams) url.searchParams.append(key, val);
-    }
-  }
-}
-
-export default class AuthSession {
-  private cookieJar: CookieJar;
-
-  readonly username: string;
-
-  private authRefreshPromise: Promise<AuthState> | null = null;
-
-  private authState: AuthState | null = null;
-
-  constructor(username: string) {
-    this.username = username;
-    this.cookieJar = new CookieJar();
-  }
-
-  public async doApiRequest(
-    reqUrl: URL | string,
-    fullOptions: RequestOptions & {
-      followRedirects?: boolean;
-      includeSessionSearchParams?: string[] | boolean;
-    },
-  ): Promise<RequestResponse> {
-    let { authState } = this;
-    if (
-      authState == null ||
-      authState.authTime + MAX_AUTH_LENGTH < Date.now()
-    ) {
-      authState = await this.refreshAuth();
-    }
-    const {
-      followRedirects,
-      includeSessionSearchParams,
-      ...options
-    } = fullOptions;
-    let url = reqUrl instanceof URL ? reqUrl : new URL(reqUrl);
-    attachAuthParams(url, authState, includeSessionSearchParams);
-    let res: RequestResponse;
-    let redirects = 0;
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      // eslint-disable-next-line no-await-in-loop
-      res = await request(url, options, this.cookieJar);
-      if (res.statusCode === 302 || res.statusCode === 301) {
-        if (res.headers.location == null) {
-          throw new Error('Invalid redirect, location header not provided.');
-        }
-        const newUrl = new URL(res.headers.location, url);
-        if (/error\.aspx$/i.test(newUrl.pathname)) {
-          if (authState.authTime + MIN_AUTH_LENGTH > Date.now()) {
-            throw new Error('Server responded with an error');
-          } else {
-            // eslint-disable-next-line no-await-in-loop
-            await this.refreshAuth();
-            // eslint-disable-next-line no-continue
-            return this.doApiRequest(reqUrl, fullOptions);
-          }
-        }
-        if (!followRedirects) {
-          break;
-        }
-        url = newUrl;
-        attachAuthParams(url, authState, includeSessionSearchParams);
-      } else {
-        break;
-      }
-      if (redirects >= MAX_REDIRECTS) {
-        throw new Error(
-          `Request failed, too many redirects: ${
-            reqUrl instanceof URL ? reqUrl.href : reqUrl
-          }`,
-        );
-      }
-      redirects += 1;
-    }
-    return res;
-  }
-
   private async doRefreshAuth(): Promise<AuthState> {
     let url: URL = new URL(AUTH_URL);
     url.searchParams.set('username', this.username);
